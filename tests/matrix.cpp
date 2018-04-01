2b9a7131ef24468c0b104d0a33335f0348498875
djabri1422 <elmezov99@gmail.com>
2018-04-01 21:42:49 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -50,82 +50,71 @@ TEST_CASE("copying matrix")
     
 TEST_CASE("adding matrix")
 {
-    std::string input
+    std::string input_A
     {
         "3, 3\n"
         "1 1 1\n"
         "2 2 2\n"
         "3 3 3" };
-    std::string input2
+    std::string input_B
     {
         "3, 3\n"
         "1 1 1\n"
         "1 1 1\n"
         "1 1 1" };
-    std::string input3
+    std::string input_C
     {
         "3, 3\n"
         "2 2 2\n"
         "3 3 3\n"
         "4 4 4" };
-    matrix_t<int> matrix1, matrix2;
-    std::istringstream istream1{ input };
-    std::istringstream istream2{ input2 };
-    REQUIRE(matrix1.read(istream1));
-    REQUIRE(matrix2.read(istream2));
-    
-    REQUIRE(matrix1.rows() == 3);
-    REQUIRE(matrix1.collumns() == 3);
-        
-    REQUIRE(matrix2.rows() == 3);
-    REQUIRE(matrix2.collumns() == 3);
 	
-    REQUIRE_NOTHROW( matrix1.collumns() == matrix2.collumns() && matrix1.rows() == matrix2.rows() );
+    matrix_t<int> A, B, C;
+    std::istringstream istream1{ input_A };
+    std::istringstream istream2{ input_B };
+    std::istringstream istream3{ input_B };
+	
+    REQUIRE(A.read(istream1));
+    REQUIRE(B.read(istream2));
+    REQUIRE(C.read(istream3));
+
+    REQUIRE_NOTHROW( A.collumns() == B.collumns() && A.rows() == B.rows() );
 	  
-    std::ostringstream stream;
-    (matrix1+matrix2).write(stream);
-    
-    REQUIRE( input3 == stream.str());
+
+    REQUIRE( ( A + B ) == C);
 }
 
 TEST_CASE("adding matrix double")
 {
-    std::string input
+    std::string input_A
     {
         "3, 3\n"
         "2.5 2.5 2.5\n"
         "2.5 2.5 2.5\n"
         "2.5 2.5 2.5" };
-    std::string input2
+    std::string input_B
     {
         "3, 3\n"
         "2.5 2.5 2.5\n"
         "2.5 2.5 2.5\n"
         "2.5 2.5 2.5" };
-    std::string input3
+    std::string input_C
     {
         "3, 3\n"
         "5 5 5\n"
         "5 5 5\n"
         "5 5 5" };
-    matrix_t<double> matrix1, matrix2;
-    std::istringstream istream1{ input };
-    std::istringstream istream2{ input2 };
-    REQUIRE(matrix1.read(istream1));
-    REQUIRE(matrix2.read(istream2));
-    
-    REQUIRE(matrix1.rows() == 3);
-    REQUIRE(matrix1.collumns() == 3);
-        
-    REQUIRE(matrix2.rows() == 3);
-    REQUIRE(matrix2.collumns() == 3);
-	
+    matrix_t<double> A, B, C;
+    std::istringstream istream1{ input_A };
+    std::istringstream istream2{ input_B };
+    std::istringstream istream3{ input_C };
+    REQUIRE(A.read(istream1));
+    REQUIRE(B.read(istream2));
+    REQUIRE(C.read(istream3));
+
     REQUIRE_NOTHROW( matrix1.collumns() == matrix2.collumns() && matrix1.rows() == matrix2.rows() );
 	  
-    std::ostringstream stream;
-    (matrix1+matrix2).write(stream);
-    
-    REQUIRE( input3 == stream.str());
+    REQUIRE( ( A + B ) == C );
 }
 
 TEST_CASE("substracting matrix")
